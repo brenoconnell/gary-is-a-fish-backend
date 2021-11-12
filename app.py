3e8ffe2629139469fad83267469eef0f69041e1e
boconne3 <boconne3@tcd.ie>
2021-11-12 12:25:17 +0000

    mongoDB credentials done correctly. Some error handling added

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import json
 from flask_cors import CORS, cross_origin
 from pymongo import MongoClient
 
-from credentials import username, pwd, dbname
+import credentials
 
 app = Flask(__name__)
 CORS(app)
 
-mongoURI = f"mongodb+srv://{username}:{pwd}@gary-sandbox.b1jde.mongodb.net/{dbname}?retryWrites=true&w=majority&authSource=admin"
+creds = credentials.Credentials.get_creds()
+
+mongoURI = f"mongodb+srv://{creds[0]}:{creds[1]}@gary-sandbox.b1jde.mongodb.net/{creds[2]}?retryWrites=true&w=majority&authSource=admin"
 
 client = MongoClient(mongoURI)
 db = client.test_drink
@@ -19,49 +21,48 @@ def get_all_drinks():
     documents = db.drinks.find()
     output = [{item: data[item] for item in data if item != '_id'} for data in documents]
     resp = jsonify(output)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route("/drink/<id>", methods=['GET'])
 @cross_origin(origin='http://localhost:3000')
 def get_drink_by_id(id):
-    
     document = db.drinks.find_one({"id": id})
     output = {item: document[item] for item in document if item != '_id'}
-
     resp = jsonify(output)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route("/drink", methods=['POST'])
 @cross_origin(origin='http://localhost:3000')
 def post_new_drink():
     content = request.get_json(force=True)
-
     result = db.drinks.insert_one(content)
+    if result.acknowledged:
+        resp = jsonify( {"status": 200, "message": "OK"} )
+    else:
+        resp = jsonify( {"status": 500, "message": "MongoDB error"} )
 
-    resp = jsonify( {"status": 200, "message": "OK"} )
-    resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route("/drink", methods=['PUT'])
 @cross_origin(origin='http://localhost:3000')
 def update_drink():
     content = request.get_json(force=True)
-
     document = db.drinks.find_one({"id": content['id']})
     new_data = {'$set': content}
 
     result = db.drinks.update_one(document, new_data)
-
-    resp = jsonify( {"status": 200, "message": "OK"} )
+    if result.acknowledged:
+        resp = jsonify( {"status": 200, "message": "OK"} )
+    else:
+        resp = jsonify( {"status": 500, "message": "MongoDB error"} )
     return resp
 
 @app.route("/drink/<id>", methods=['DELETE'])
 @cross_origin(origin='http://localhost:3000')
 def delete_drink(id):
-
     result = db.drinks.delete_one({"id": id})
-
-    resp = jsonify( {"status": 200, "message": "OK"} )
+    if result.acknowledged:
+        resp = jsonify( {"status": 200, "message": "OK"} )
+    else:
+        resp = jsonify( {"status": 500, "message": "MongoDB error"} )
     return resp
