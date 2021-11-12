bcb61ccb462adbac963b9653cb7b34ac4b8755c2
boconne3 <boconne3@tcd.ie>
2021-11-12 01:16:56 +0000

    Update method added to API

--- app.py
+++ app.py
@@ -11,9 +11,6 @@ db = client.test_drink
 
 @app.route("/drink", methods=['GET'])
 def get_all_drinks():
-    # f = open('allDrinks.json',)
-    # allDrinks = json.load(f)
-    # f.close()
     documents = db.drinks.find()
     output = [{item: data[item] for item in data if item != '_id'} for data in documents]
     resp = jsonify(output)
@@ -22,69 +19,42 @@ def get_all_drinks():
 
 @app.route("/drink/<id>", methods=['GET'])
 def get_drink_by_id(id):
-    f = open('allDrinks.json',)
-    allDrinks = json.load(f)
-    f.close()
-    respDrink = None
+    
+    document = db.drinks.find_one({"id": id})
+    output = {item: document[item] for item in document if item != '_id'}
 
-    for drink in allDrinks:
-        for attribute, value in drink.items():
-            if(attribute == "id" and value == id):
-                respDrink = drink
-                break
-    
-    if(not respDrink == None):
-        resp = jsonify(respDrink)
-    else:
-        resp = jsonify()
-    
+    resp = jsonify(output)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 @app.route("/drink", methods=['POST'])
 def post_new_drink():
     content = request.get_json(force=True)
-    # json_object = json.dumps(content, indent = 4)
 
-    # fin = open("allDrinks.json", "rt")
-    # data = fin.read()
-    # data = data.replace(']', '')
-    # fin.close()
-
-    # fin = open("allDrinks.json", "wt")
-    # fin.write(data)
-    # fin.close()
-
-    # with open("allDrinks.json", "a+") as outfile:
-    #     outfile.write(",\n" + json_object + "\n]")
-    # outfile.close()
-
-    result = db.drink.insert_one(content)
+    result = db.drinks.insert_one(content)
 
     resp = jsonify( {"status": 200, "message": f"OK - New ID: {result.inserted_id}"} )
     resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+@app.route("/drink", methods=['PUT'])
+@cross_origin(origin='http://localhost:3000')
+def update_drink():
+    content = request.get_json(force=True)
+
+    document = db.drinks.find_one({"id": content['id']})
+    new_data = {'$set': content}
+
+    result = db.drinks.update_one(document, new_data)
+
+    resp = jsonify( {"status": 200, "message": "OK"} )
     return resp
 
 @app.route("/drink/<id>", methods=['DELETE'])
 @cross_origin(origin='http://localhost:3000')
 def delete_drink(id):
 
-    f = open('allDrinks.json',)
-    allDrinks = json.load(f)
-    indexToRemove = -1
-    for i in range(len(allDrinks)):
-        if(allDrinks[i]['id'] == id):
-            indexToRemove = i
-            break
-    if(not indexToRemove > -1):
-        resp = jsonify( {"status": 404, "message": "File not found"} )
-        return resp
-
-    allDrinks.pop(indexToRemove)
-    f.close()
-    f = open('allDrinks.json', 'w')
-    f.write(json.dumps(allDrinks, sort_keys=True, indent=4, separators=(',', ': ')))
-    f.close()
+    result = db.drinks.delete_one({"id": id})
 
     resp = jsonify( {"status": 200, "message": "OK"} )
     return resp
